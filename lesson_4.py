95b180573e756405b8e7ec888ed1a654a5b9e238
enin-cloud <dewea.dewea.200@gmail.com>
2024-12-10 14:26:25 +0300

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -1,1 +1,24 @@
+#1
+s = input("Введите строчку: ")
+k=m=0
+for i in range(len(s)):
+    if s[i]=='н':
+        k += 1
+    else:
+        m=max(m,k)
+        k=0
+s=s.replace('н','!')
+print("Максимальное каличество <<н>> подряд: ", m,"\n Новая строчка: ", s)
 
+#2
+s = input("Введите строчку: ")
+for i in range(len(s)):
+    if s[i] == "{" or s[i] == "[" or s[i] == "(":
+        x = i
+    elif s[i] == "}" or s[i] == "]" or s[i] == ")":
+        y = i
+print(s[x+1:y])
+s=input("Введите строчку: ").split()
+for i in s:
+    if i[0]=="а" and i[-1]=="я":
+        print("Слова: ", i)
