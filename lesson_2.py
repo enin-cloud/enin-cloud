4ba1adac15059f5fc2446cd682adcab5b4cd635b
enin-cloud <dewea.dewea.200@gmail.com>
2024-12-10 14:21:33 +0300

    Update lesson_2.py

--- lesson_2.py
+++ lesson_2.py
@@ -1,3 +1,4 @@
+#1
 n = int(input())
 n > 0
 if n>100:
@@ -6,6 +7,7 @@ else:
     for i in range(0 , n+1):
         print(i**2)
 
+#2
 for i in range(1, 10):
     for j in range(1, 10):
         print(i*j)
